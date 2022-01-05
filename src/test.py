de07d4523129114b71c6edaec1e79de06f5b5915
gabrielefantini <gabrielefantini97@gmail.com>
2022-01-05 18:44:10 +0100

    added tests

--- src/test.py
+++ src/test.py
@@ -14,13 +14,27 @@ from torch.autograd import Variable
 sys.path.append('.')  # noqa: E402
 from model import RACNN
 from plant_loader import get_plant_loader
-from pretrain_apn import random_sample, log, clean, save_img, build_gif
+from pretrain_apn import log, clean, save_img, build_gif
+
+def random_sample(dataloader):
+    for batch_idx, (inputs, labels) in enumerate(dataloader, 1):
+        return [inputs[0].cuda(), labels[0].cuda()]
 
 def run(pretrained_model):
     accuracy = 0
     net = RACNN(num_classes=6).cuda()
     net.load_state_dict(torch.load(pretrained_model))
-    
+
+    data_set = get_plant_loader()
+    validationloader = torch.utils.data.DataLoader(data_set["validation"], batch_size=32, shuffle=False)
+    sample = random_sample(validationloader)
+    preds, _, _, resized = net(sample[0].unsqueeze(0))
+    x1, x2 = resized[0].data, resized[1].data
+    save_img(sample[0].unsqueeze(0), path=f'build/.cache/original.jpg', annotation=f'test')
+    save_img(x1, path=f'build/.cache/test@2x.jpg', annotation=f'test')
+    save_img(x2, path=f'build/.cache/test@4x.jpg', annotation=f'test')
+    print(sample[1], preds)
+
 
 
 if __name__ == "__main__":

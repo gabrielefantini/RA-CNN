1db768e1b0b87f11b93dae775555cae671da2509
Lorenzo <lorenzoappendini@gmail.com>
2022-01-23 15:41:00 +0100

    refactor save to csv

--- src/forge.py
+++ src/forge.py
@@ -16,7 +16,9 @@ from model import RACNN
 from plant_loader import get_plant_loader
 from pretrain_apn import random_sample, log, clean, save_img, build_gif
 
+
 def avg(x): return sum(x)/len(x)
+
 
 def train(net, dataloader, optimizer, epoch, _type):
     assert _type in ['apn', 'backbone']
@@ -35,7 +37,7 @@ def train(net, dataloader, optimizer, epoch, _type):
 
 
 def test(net, dataloader):
-    correct = [0,0,0]
+    correct = [0, 0, 0]
     cnt = 0
     avg_accuracy = 0
 
@@ -46,15 +48,16 @@ def test(net, dataloader):
             outputs, _, _, _ = net(inputs)
             for idx, logits in enumerate(outputs):
                 logits = torch.sigmoid(logits)
-                logits[logits >= 0.5 ] = 1
-                logits[logits < 0.5 ] = 0
-                #correct_summary[f'clsf-{idx}']['top-1'] +=  torch.all(torch.eq(logits, labels),  dim=1).sum()  # top-1
-                correct[idx] += torch.all(torch.eq(logits, labels),  dim=1).sum()
-                #correct_summary[f'clsf-{idx}']['top-5'] += torch.eq(logits.topk(max((1, 5)), 1, True, True)[1], labels.view(-1, 1)).sum().float().item()  # top-5
-    for id,value in enumerate(correct):
-      correct[id] = value/cnt
-      avg_accuracy += correct[id]
-    
+                logits[logits >= 0.5] = 1
+                logits[logits < 0.5] = 0
+                # correct_summary[f'clsf-{idx}']['top-1'] +=  torch.all(torch.eq(logits, labels),  dim=1).sum()  # top-1
+                correct[idx] += torch.all(torch.eq(logits,
+                                          labels),  dim=1).sum()
+                # correct_summary[f'clsf-{idx}']['top-5'] += torch.eq(logits.topk(max((1, 5)), 1, True, True)[1], labels.view(-1, 1)).sum().float().item()  # top-5
+    for id, value in enumerate(correct):
+        correct[id] = value/cnt
+        avg_accuracy += correct[id]
+
     return avg_accuracy/3, correct
 
 
@@ -65,64 +68,45 @@ def run(pretrained_model):
     net.load_state_dict(torch.load(pretrained_model))
     cudnn.benchmark = True
 
-    #ognuna delle 3 cnn del modello parte con i valori della cnn pre addestrata e poi ognuna si specializza
-    #con i propri parametri
+    # ognuna delle 3 cnn del modello parte con i valori della cnn pre addestrata e poi ognuna si specializza
+    # con i propri parametri
     cls_params = list(net.b1.parameters()) + list(net.b2.parameters()) + list(net.b3.parameters()) + \
-        list(net.classifier1.parameters()) + list(net.classifier2.parameters()) + list(net.classifier3.parameters())
+        list(net.classifier1.parameters()) + \
+        list(net.classifier2.parameters()) + list(net.classifier3.parameters())
     apn_params = list(net.apn1.parameters()) + list(net.apn2.parameters())
 
     cls_opt = optim.SGD(cls_params, lr=0.001)
-    #TODO da modificare in lr=1e-6
+    # TODO da modificare in lr=1e-6
     apn_opt = optim.SGD(apn_params, lr=1e-6)
 
     data_set = get_plant_loader()
-    trainloader = torch.utils.data.DataLoader(data_set["train"], batch_size=16, shuffle=True)
-    validationloader = torch.utils.data.DataLoader(data_set["validation"], batch_size=16, shuffle=False)
+    trainloader = torch.utils.data.DataLoader(
+        data_set["train"], batch_size=16, shuffle=True)
+    validationloader = torch.utils.data.DataLoader(
+        data_set["validation"], batch_size=16, shuffle=False)
     sample = random_sample(validationloader)
 
-    cls_losses = []
-    rank_losses = []
-    training_accuracies = []
-    validation_accuracies = []
-
-    cls1_accuracies_valid = []
-    cls2_accuracies_valid = []
-    cls3_accuracies_valid = []
-
-    cls1_accuracies_train = []
-    cls2_accuracies_train = []
-    cls3_accuracies_train = []
-
-    #15
+    # 15
     for epoch in range(20):
-        
+
         cls_loss = train(net, trainloader, cls_opt, epoch, 'backbone')
         rank_loss = train(net, trainloader, apn_opt, epoch, 'apn')
-        cls_losses.append(cls_loss)
-        rank_losses.append(rank_loss)
 
         log(' :: Testing on validation set ...')
         temp_accuracy, valid_corrects = test(net, validationloader)
-        validation_accuracies.append(temp_accuracy)
-        cls1_accuracies_valid.append(valid_corrects[0])
-        cls2_accuracies_valid.append(valid_corrects[1])
-        cls3_accuracies_valid.append(valid_corrects[2])
 
-        print("validation accuracies:" , valid_corrects)
+        print("validation accuracies:", valid_corrects)
 
         log(' :: Testing on training set ...')
         train_accuracy, train_corrects = test(net, trainloader)
-        training_accuracies.append(train_accuracy)
-        cls1_accuracies_train.append(train_corrects[0])
-        cls2_accuracies_train.append(train_corrects[1])
-        cls3_accuracies_train.append(train_corrects[2])
-
 
         # visualize cropped inputs
         _, _, _, resized = net(sample.unsqueeze(0))
         x1, x2 = resized[0].data, resized[1].data
-        save_img(x1, path=f'build/.cache/epoch_{epoch}@2x.jpg', annotation=f'cls_loss = {cls_loss:.7f}, rank_loss = {rank_loss:.7f}')
-        save_img(x2, path=f'build/.cache/epoch_{epoch}@4x.jpg', annotation=f'cls_loss = {cls_loss:.7f}, rank_loss = {rank_loss:.7f}')
+        save_img(x1, path=f'build/.cache/epoch_{epoch}@2x.jpg',
+                 annotation=f'cls_loss = {cls_loss:.7f}, rank_loss = {rank_loss:.7f}')
+        save_img(x2, path=f'build/.cache/epoch_{epoch}@4x.jpg',
+                 annotation=f'cls_loss = {cls_loss:.7f}, rank_loss = {rank_loss:.7f}')
 
         # save model per 10 epoches
         if temp_accuracy > accuracy:
@@ -133,27 +117,45 @@ def run(pretrained_model):
             torch.save(cls_opt.state_dict(), f'build/cls_optimizer.pt')
             torch.save(apn_opt.state_dict(), f'build/apn_optimizer.pt')
 
-    np.savetxt(f'logs/racnn-cls-loss.csv',cls_losses,'%.10f',',')
-    np.savetxt(f'logs/racnn-rank-loss.csv',rank_losses,'%.10f',',')
-    np.savetxt(f'logs/racnn-training-accuracy.csv',training_accuracies,'%.10f',',')
-    np.savetxt(f'logs/racnn-validation-accuracy.csv',validation_accuracies,'%.10f',',')
-    np.savetxt(f'logs/racnn-cls1-validation-accuracy.csv',cls1_accuracies_valid,'%.10f',',')
-    np.savetxt(f'logs/racnn-cls2-validation-accuracy.csv',cls2_accuracies_valid,'%.10f',',')
-    np.savetxt(f'logs/racnn-cls3-validation-accuracy.csv',cls3_accuracies_valid,'%.10f',',')
-    np.savetxt(f'logs/racnn-cls1-training-accuracy.csv',cls1_accuracies_train,'%.10f',',')
-    np.savetxt(f'logs/racnn-cls2-training-accuracy.csv',cls2_accuracies_train,'%.10f',',')
-    np.savetxt(f'logs/racnn-cls3-training-accuracy.csv',cls3_accuracies_train,'%.10f',',')
-      
-      
+        # save outputs to csv files
+        saveFieldToFile(cls_loss, f'logs/racnn-cls-loss.csv')
+        saveFieldToFile(rank_loss, f'logs/racnn-rank-loss.csv')
+        saveFieldToFile(temp_accuracy, f'logs/racnn-validation-accuracy.csv')
+        saveFieldToFile(
+            valid_corrects[0], f'logs/racnn-cls1-validation-accuracy.csv')
+        saveFieldToFile(
+            valid_corrects[1], f'logs/racnn-cls2-validation-accuracy.csv')
+        saveFieldToFile(
+            valid_corrects[2], f'logs/racnn-cls3-validation-accuracy.csv')
+        saveFieldToFile(train_accuracy, f'logs/racnn-training-accuracy.csv')
+        saveFieldToFile(train_corrects[0],
+                        f'logs/racnn-cls1-training-accuracy.csv')
+        saveFieldToFile(train_corrects[1],
+                        f'logs/racnn-cls2-training-accuracy.csv')
+        saveFieldToFile(train_corrects[2],
+                        f'logs/racnn-cls3-training-accuracy.csv')
+
+
+def saveFieldToFile(newfield, filename):
+    with open(filename, 'a') as f:
+        np.savetxt(f, [newfield], '%.10f', ',')
+
+
+def cleanlog():
+    path = 'logs'
+    if os.path.exists(path):
+        shutil.rmtree(path)
+    os.makedirs(path)
 
 
 if __name__ == "__main__":
     clean()
     os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-    path = 'logs'
-    if not os.path.exists(path):
-        os.makedirs(path)
-    #RACNN con backbone e apn pre addestrate
+    cleanlog()  # dato che ora viene fatto append dei file, se non li elimini dopo ogni run accumula dati
+    # path = 'logs'
+    # if not os.path.exists(path):
+    #     os.makedirs(path)
+    # RACNN con backbone e apn pre addestrate
     run(pretrained_model='build/racnn_pretrained.pt')
     build_gif(pattern='@2x', gif_name='racnn_efficientNet')
     build_gif(pattern='@4x', gif_name='racnn_efficientNet')
